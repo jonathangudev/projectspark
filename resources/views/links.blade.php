5296fcc5f423743f57a4de4d56cf7b9783624e4c
johnguiscool <johnguiscool@gmail.com>
2019-05-22 10:20:09 -0400

    links

--- resources/views/links.blade.php
+++ resources/views/links.blade.php
@@ -1,74 +1,65 @@
-@extends('layouts.layout')
-
-@section('title')
- - Links
-@endsection
-
-
-@section('css')
-<link href="css/extra-styles.css" rel="stylesheet">
-@endsection
-
+@extends('spark::layouts.app')
 
 @section('content')
 
-    		<div class="other-resources-links">
-    			<h1 class="text-center">Other Resources for Learning Ukrainian</h1>
 
-    			<ul>
-    			<li>The Peace Corps Ukrainian Language Course at Live Lingua:<br>
-    			<a href="https://www.livelingua.com/project/peace-corps/Ukrainian/">www.livelingua.com/project/peace-corps/Ukrainian/</a>
-    			</li>
+<div class="other-resources-links">
+	<h1 class="text-center">Other Resources for Learning Ukrainian</h1>
 
-    			<li>A blog with Ukrainian Lessons:<br>
-    			<a href="http://ukrainianlessons.com/">ukrainianlessons.com</a>
-    			</li>
+	<ul>
+		<li>The Peace Corps Ukrainian Language Course at Live Lingua:<br>
+			<a href="https://www.livelingua.com/project/peace-corps/Ukrainian/">www.livelingua.com/project/peace-corps/Ukrainian/</a>
+		</li>
 
-    			<li>Read Ukrainian!  A free course for learning to read Ukrainian texts:<br>
-    			<a href="http://www.ukrainianlanguage.org.uk/read/">http://www.ukrainianlanguage.org.uk/read/</a>
-    			</li>
+		<li>A blog with Ukrainian Lessons:<br>
+			<a href="http://ukrainianlessons.com/">ukrainianlessons.com</a>
+		</li>
 
-    			</ul>
-    		</div>
+		<li>Read Ukrainian! A free course for learning to read Ukrainian texts:<br>
+			<a href="http://www.ukrainianlanguage.org.uk/read/">http://www.ukrainianlanguage.org.uk/read/</a>
+		</li>
 
-    		<div class"media-resources-links">
-    			<h1 class="text-center">Ukrainian-Language Media</h1>
-    			<ul>
-    			<li>Ukrainian and Russian Language Television Channel:<br>
-    			<a href="http://1plus1.ua">1plus1.ua</a>
-    			</li>
+	</ul>
+</div>
 
-    			</ul>
+<div class"media-resources-links">
+	<h1 class="text-center">Ukrainian-Language Media</h1>
+	<ul>
+		<li>Ukrainian and Russian Language Television Channel:<br>
+			<a href="http://1plus1.ua">1plus1.ua</a>
+		</li>
 
-    		</div>
+	</ul>
 
-    		<div class="partners-links">
-    		<h1 class="text-center">Online Language Partners and Teachers</h1>
+</div>
 
-    			The best way to learn a language is by having real conversations with native speakers.
-    			Thanks to websites like Preply, it's very easy to find Ukrainian speakers to talk with online for quite cheap (sometimes for as little as $5 an hour).
-    			We highly recommend you practice your Ukrainian by getting in contact with teachers from these websites.
+<div class="partners-links">
+	<h1 class="text-center">Online Language Partners and Teachers</h1>
 
-    			<ul>
-    			<li>Preply:<br>
-    			<a href="https://preply.com">preply.com</a>
-    			</li>
+	The best way to learn a language is by having real conversations with native speakers.
+	Thanks to websites like Preply, it's very easy to find Ukrainian speakers to talk with online for quite cheap (sometimes for as little as $5 an hour).
+	We highly recommend you practice your Ukrainian by getting in contact with teachers from these websites.
 
-    			<li>italki:<br>
-    			<a href="https://www.italki.com/">italki.com</a>
-    			</li>
+	<ul>
+		<li>Preply:<br>
+			<a href="https://preply.com">preply.com</a>
+		</li>
 
-    			</ul>
-    		</div>
+		<li>italki:<br>
+			<a href="https://www.italki.com/">italki.com</a>
+		</li>
 
-    		<div class="courses-in-ukraine-links">
-    		<h1 class="text-center">Summer Programs and Courses in Ukraine</h1>
-    			<ul>
-    			<li>Summer Ukrainian Program at the Ukrainian Catholic University in Lviv<br>
-    			<a href="http://studyukrainian.org.ua/">studyukrainian.org.ua</a></li>
+	</ul>
+</div>
 
-    			<li>Ukrainian Language and Culture School in Lviv<br>
-    			<a href="http://learn-ukrainian.org.ua/">learn-ukrainian.org.ua</a></li>
+<div class="courses-in-ukraine-links">
+	<h1 class="text-center">Summer Programs and Courses in Ukraine</h1>
+	<ul>
+		<li>Summer Ukrainian Program at the Ukrainian Catholic University in Lviv<br>
+			<a href="http://studyukrainian.org.ua/">studyukrainian.org.ua</a></li>
+
+		<li>Ukrainian Language and Culture School in Lviv<br>
+			<a href="http://learn-ukrainian.org.ua/">learn-ukrainian.org.ua</a></li>
 
 
-@endsection
+		@endsection
