ac8e065d65a2291228f1241b1c94050006a4674a
johnguiscool <johnguiscool@gmail.com>
2019-05-22 11:30:45 -0400

    guest nav links

--- resources/views/vendor/spark/nav/guest.blade.php
+++ resources/views/vendor/spark/nav/guest.blade.php
@@ -26,11 +26,15 @@
                 </g>
             </svg>
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div id="navbarSupportedContent" class="collapse navbar-collapse">
+
+            <ul class="navbar-nav mr-auto">
+                @includeIf('spark::nav.user-left')
+            </ul>
+
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
                     <a class="nav-link" href="/login">{{__('Login')}}</a>
