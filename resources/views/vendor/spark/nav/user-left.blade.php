ce71c1e074528a8f9f992be6f5243b075565f21e
johnguiscool <johnguiscool@gmail.com>
2019-05-22 11:19:49 -0400

    added links

--- resources/views/vendor/spark/nav/user-left.blade.php
+++ resources/views/vendor/spark/nav/user-left.blade.php
@@ -5,3 +5,19 @@
     <a class="nav-link" href="#">Your Link</a>
 </li>
 -->
+
+<li class="nav-item">
+    <a class="nav-link" href={{ URL::to('lessons') }}>Lessons</a>
+</li>
+
+<li class="nav-item">
+    <a class="nav-link" href={{ URL::to('grammar') }}>Grammar</a>
+</li>
+
+<li class="nav-item">
+    <a class="nav-link" href={{ URL::to('about') }}>About</a>
+</li>
+
+<li class="nav-item">
+    <a class="nav-link" href={{ URL::to('blog') }}>Blog</a>
+</li>
