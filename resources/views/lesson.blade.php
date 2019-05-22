81472fdc8c5f513ca663167305522123143bcba4
johnguiscool <johnguiscool@gmail.com>
2019-05-22 10:52:40 -0400

    stuff

--- resources/views/lesson.blade.php
+++ resources/views/lesson.blade.php
@@ -8,8 +8,10 @@ Study Ukrainian - Ukrainian Lesson <?php echo $dialogue_number; ?>
 <link href="../../css/sticky-footer-navbar.css" rel="stylesheet"> <!-- from: http://getbootstrap.com/docs/4.0/examples/sticky-footer-navbar/# -->
 <link href="../../css/tooltip.css" rel="stylesheet">
 <link href="../../css/extra-styles.css" rel="stylesheet">
+@endsection
 
 
+@push('scripts')
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 
 <script>
@@ -48,12 +50,9 @@ Study Ukrainian - Ukrainian Lesson <?php echo $dialogue_number; ?>
     //}
   }
 </script>
-
-@endsection
-
+@endpush
 
 <?php $lesson = new Lesson($dialogue_number); ?>
-
 
 
 @section('content')
