da5f3f7e07528974a71ef8549d5d4df132d031f3
johnguiscool <johnguiscool@gmail.com>
2019-05-26 12:00:16 -0400

    tweaks

--- resources/views/premium.blade.php
+++ resources/views/premium.blade.php
@@ -18,18 +18,26 @@ Study Ukrainian - Premium
             <div class="row justify-content-around">
 
                 <div class="col-12 ">
-                    Study Ukrainian Premium comes with the following features:
+                    <h2>Study Ukrainian Premium comes with the following features:</h2>
 
                     <ul>
-                        <li>Full access to 25 lessons of the Ukrainian Language</li>
-                        <li>Fully-voiced audio by native speakers</li>
-                        <li>Grammar Explanations for each lesson</li>
+                        <li>
+                            <h3>Full access to 25 lessons of the Ukrainian Language<h3>
+                        </li>
+                        <li>
+                            <h3>Fully-voiced audio by native speakers<h3>
+                        </li>
+                        <li>
+                            <h3>Grammar Explanations for each lesson<h3>
+                        </li>
                     </ul>
 
-                    <button class="button btn-primary">Sign Up Now</button>
+                    <h3>Price: $3.99 per month</h3>
+
+                    <button class="button btn-primary btn-lg">Sign Up Now</button>
                 </div>
 
             </div>
-        </div>>
+        </div>S
     </div>
 </home> @endsection
